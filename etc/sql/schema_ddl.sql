826e0dc234c11105a1c427981bb4095372eb9ffb
K <x00.x7f@gmail.com>
2017-05-13 21:50:07 +0900

    Updated sql schema by migrate script

--- etc/sql/schema_ddl.sql
+++ etc/sql/schema_ddl.sql
@@ -11,16 +11,28 @@ CREATE INDEX "category_idx" on "thread" ("category");
 -- Table: writer
 --
 CREATE TABLE "writer" (
+  "id" serial NOT NULL,
   "thread_id" integer NOT NULL,
-  "id" character varying(25) NOT NULL,
   "name" character varying(255) NOT NULL,
   "email" character varying(40) NOT NULL,
-  "message" text NOT NULL,
-  "abone" smallint DEFAULT 0 NOT NULL,
   "created_at" timestamp DEFAULT 'now()' NOT NULL,
   PRIMARY KEY ("thread_id")
 );
 CREATE INDEX "thread_id_idx" on "writer" ("thread_id");
+
+--
+-- Table: message
+--
+CREATE TABLE "message" (
+  "id" serial NOT NULL,
+  "writer_id" integer NOT NULL,
+  "text" text NOT NULL,
+  "abone" smallint DEFAULT 0 NOT NULL,
+  "created_at" timestamp DEFAULT 'now()' NOT NULL,
+  "updated_at" timestamp DEFAULT 'now()' NOT NULL,
+  PRIMARY KEY ("writer_id")
+);
+CREATE INDEX "message_id_idx" on "message" ("id");
 
 --
 -- Foreign Key Definitions
@@ -31,4 +43,10 @@ ALTER TABLE "thread" ADD FOREIGN KEY ("id")
 
 ALTER TABLE "writer" ADD FOREIGN KEY ("thread_id")
   REFERENCES "thread" ("id") DEFERRABLE;
+
+ALTER TABLE "writer" ADD FOREIGN KEY ("id")
+  REFERENCES "message" ("writer_id") DEFERRABLE;
+
+ALTER TABLE "message" ADD FOREIGN KEY ("writer_id")
+  REFERENCES "writer" ("id") DEFERRABLE;
 
--- lib/BBS/Model/Schema.pm
+++ lib/BBS/Model/Schema.pm
@@ -7,7 +7,7 @@ create_table 'thread' => columns {
     integer   'id',         primary_key, unsigned, auto_increment;
     varchar   'name',       not_null;
     varchar   'category',   not_null;
-    timestamp 'created_at', not_null;
+    timestamp 'created_at', not_null, default => 'now()';
 
     has_many 'writer';
 
@@ -15,18 +15,32 @@ create_table 'thread' => columns {
 };
 
 create_table 'writer' => columns {
+    integer   'id',         primary_key, unsigned, auto_increment;
     integer   'thread_id',  primary_key;
-    varchar   'id',         not_null, size => 25;
     varchar   'name',       not_null;
     varchar   'email',      not_null, size => 40;
-    text      'message',    not_null;
-    timestamp 'created_at', not_null;
+    timestamp 'created_at', not_null, default => 'now()';
 
     # 外部キーの制約を行うことで, thread.id に格納さている
     # id 以外の数値を writer.thread_id へ格納することができない
     belongs_to 'thread';
 
+    has_many 'message';
+
     add_index 'thread_id_idx' => ['thread_id'];
+};
+
+create_table 'message' => columns {
+    integer   'id',         primary_key, unsigned, auto_increment;
+    integer   'writer_id',  primary_key;
+    text      'text',       not_null;
+    tinyint   'abone',      not_null, default => 0;
+    timestamp 'created_at', not_null, default => 'now()';
+    timestamp 'updated_at', not_null, default => 'now()';
+
+    belongs_to 'writer';
+
+    add_index 'message_id_idx' => ['id'];
 };
 
 1;
