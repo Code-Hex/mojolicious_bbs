97f60d51f231896ec37d53aaecf77a31538b370d
K <x00.x7f@gmail.com>
2017-05-13 15:54:14 +0900

    Added Schema

---
+++ etc/sql/schema_ddl.sql
@@ -0,0 +1,33 @@
+CREATE TABLE "thread" (
+  "id" serial NOT NULL,
+  "name" character varying(255) NOT NULL,
+  "category" character varying(255) NOT NULL,
+  "created_at" timestamp NOT NULL,
+  PRIMARY KEY ("id")
+);
+CREATE INDEX "category_idx" on "thread" ("category");
+
+--
+-- Table: writer
+--
+CREATE TABLE "writer" (
+  "thread_id" integer NOT NULL,
+  "id" character varying(25) NOT NULL,
+  "name" character varying(255) NOT NULL,
+  "email" character varying(40) NOT NULL,
+  "message" text NOT NULL,
+  "created_at" timestamp NOT NULL,
+  PRIMARY KEY ("thread_id")
+);
+CREATE INDEX "thread_id_idx" on "writer" ("thread_id");
+
+--
+-- Foreign Key Definitions
+--
+
+ALTER TABLE "thread" ADD FOREIGN KEY ("id")
+  REFERENCES "writer" ("thread_id") DEFERRABLE;
+
+ALTER TABLE "writer" ADD FOREIGN KEY ("thread_id")
+  REFERENCES "thread" ("id") DEFERRABLE;
+
