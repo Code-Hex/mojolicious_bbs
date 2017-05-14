86a947a6fa6dd39951e68746fa2ae4fdd96c47d7
K <x00.x7f@gmail.com>
2017-05-14 16:09:21 +0900

    Updated sql schema by migrate script

--- etc/sql/schema_ddl.sql
+++ etc/sql/schema_ddl.sql
@@ -1,3 +1,14 @@
+CREATE TABLE "writer" (
+  "id" serial NOT NULL,
+  "name" character varying(255) NOT NULL,
+  "email" character varying(40) NOT NULL,
+  "created_at" timestamp NOT NULL,
+  PRIMARY KEY ("id")
+);
+
+--
+-- Table: thread
+--
 CREATE TABLE "thread" (
   "id" serial NOT NULL,
   "name" character varying(255) NOT NULL,
@@ -8,29 +19,17 @@ CREATE TABLE "thread" (
 CREATE INDEX "category_idx" on "thread" ("category");
 
 --
--- Table: writer
---
-CREATE TABLE "writer" (
-  "id" serial NOT NULL,
-  "thread_id" integer NOT NULL,
-  "name" character varying(255) NOT NULL,
-  "email" character varying(40) NOT NULL,
-  "created_at" timestamp NOT NULL,
-  PRIMARY KEY ("thread_id")
-);
-CREATE INDEX "thread_id_idx" on "writer" ("thread_id");
-
---
 -- Table: message
 --
 CREATE TABLE "message" (
   "id" serial NOT NULL,
-  "writer_id" integer NOT NULL,
   "text" text NOT NULL,
   "abone" smallint DEFAULT 0 NOT NULL,
+  "writer_id" integer NOT NULL,
+  "thread_id" integer NOT NULL,
   "created_at" timestamp NOT NULL,
   "updated_at" timestamp NOT NULL,
-  PRIMARY KEY ("writer_id")
+  PRIMARY KEY ("thread_id")
 );
 CREATE INDEX "message_id_idx" on "message" ("id");
 
@@ -38,15 +37,9 @@ CREATE INDEX "message_id_idx" on "message" ("id");
 -- Foreign Key Definitions
 --
 
-ALTER TABLE "thread" ADD FOREIGN KEY ("id")
-  REFERENCES "writer" ("thread_id") DEFERRABLE;
-
-ALTER TABLE "writer" ADD FOREIGN KEY ("thread_id")
-  REFERENCES "thread" ("id") DEFERRABLE;
-
-ALTER TABLE "writer" ADD FOREIGN KEY ("id")
-  REFERENCES "message" ("writer_id") DEFERRABLE;
-
 ALTER TABLE "message" ADD FOREIGN KEY ("writer_id")
   REFERENCES "writer" ("id") DEFERRABLE;
+
+ALTER TABLE "message" ADD FOREIGN KEY ("thread_id")
+  REFERENCES "thread" ("id") DEFERRABLE;
 
